af7f2762fc735f10878e8f9b918530c98276970d
Fernando <fernando11554@yahoo.com>
2016-04-10 23:36:31 -0400

    HTML Landing Page and Registration Page

--- src/main/java/softeng/Main.java
+++ src/main/java/softeng/Main.java
@@ -1,9 +1,31 @@
 package softeng;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (request, response) -> "Hello World");
+
+        staticFileLocation("/public");
+
+        /*
+            Home Page Route
+         */
+        get("/", (request, response) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        /*
+            Registration Route
+         */
+        get("/registration", (request, response) -> {
+            return new ModelAndView(null, "registration.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }

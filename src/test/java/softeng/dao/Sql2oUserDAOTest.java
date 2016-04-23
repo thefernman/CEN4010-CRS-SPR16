d8e9d8a0e3de9ab4fcb70a9216a9ae7cb044228b
Fernando <fernando11554@yahoo.com>
2016-04-23 15:05:43 -0400

    More Backend work . FCC

---
+++ src/test/java/softeng/dao/Sql2oUserDAOTest.java
@@ -0,0 +1,13 @@
+package softeng.dao;
+
+import org.sql2o.Connection;
+import softeng.dao.users.Sql2oUserDAO;
+
+/**
+ * Created by Fernando on 4/23/2016.
+ */
+public class Sql2oUserDAOTest {
+
+    private Sql2oUserDAO userDAO;
+    private Connection conn;
+}
